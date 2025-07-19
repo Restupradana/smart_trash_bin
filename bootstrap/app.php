10d26d9ca5f16e78aa3df10b245f54e33cd4dddc
Puji Restu Pradana <149686637+Restupradana@users.noreply.github.com>
2025-07-19 12:22:48 +0700

    Update
    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,11 @@ use App\Http\Middleware\RoleMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php', // ✅ tambahkan baris ini
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
